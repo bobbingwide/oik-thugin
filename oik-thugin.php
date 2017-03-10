9397cd1f155cede308c97b0418eb4ad15d169026
Herb Miller <herb@bobbingwide.com>
2017-03-10 11:02:02 +0000

    v0.0.0 2017/03/10 - based on the bwdesign pseudo grandchild theme

---
+++ oik-thugin.php
@@ -0,0 +1,145 @@
+<?php 
+/**
+Plugin Name: oik-thugin
+Depends: oik base plugin, oik fields, oik themes, oik-shortcodes
+Plugin URI: http://www.bobbingwide.com/blog/oik_plugins/oik-thugin
+Description: Letter taxonomies for oik-plugins.com	- pseudo grandchild theme
+Version: 0.0.0
+Author: bobbingwide
+Author URI: http://www.oik-plugins.com/author/bobbingwide
+Text Domain: oik_thugin
+Domain Path: /languages/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+    Copyright 2017 Bobbing Wide (email : herb@bobbingwide.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+
+/**
+ * Register the additional taxonomies for oik_thugin
+ *
+ * - Depends on oik-a2z for the "letters" taxonomy.
+ * - Depends on oik-shortcodes-a2z for the "oik_letters" taxonomy. 
+ * - oik-a2z automatically registers the filter that will set the taxonomy term from the title or content. 
+ *
+ */ 
+function oik_thugin_loaded() {
+  add_action( 'oik_fields_loaded', 'oik_thugin_oik_fields_loaded', 16 );
+	add_action( "wp_enqueue_scripts", "oik_thugin_enqueue_scripts", 12 );
+	add_filter( 'genesis_footer_creds_text', "oik_thugin_genesis_footer_creds_text", 11 );
+	//add_action( 'genesis_entry_footer', 'oik_thugin_genesis_entry_footer', 11 );
+	//add_filter( "register_post_type_args", "oik_thugin_register_post_type_args", 10, 2 );
+}
+
+/**
+ * Implements 'oik_fields_loaded' for oik-plugins.com / oik-plugins.co.uk
+ *
+ * * Registers the letters taxonomy for oik plugins, themes, shortcodes and FAQs
+ * * Registers the _plugin_ref for posts, pages and FAQs
+ *
+ * Note: The association of a post type to a letter taxonomy will automatically set the 
+ * filter hooks which automatically set the taxonomy terms for a post
+ * from the title and/or content. 
+ * 
+ */ 
+function oik_thugin_oik_fields_loaded() {
+	register_taxonomy_for_object_type( "letters", "page" ); 
+	bw_register_field_for_object_type( "letters", "page" );
+	
+	bw_register_custom_tags( "letters", "oik-plugins", "Letters" );
+	bw_register_field_for_object_type( "letters", "oik-plugins" );
+	
+	register_taxonomy_for_object_type( "letters", "oik-themes" );
+	bw_register_field_for_object_type( "letters", "oik-themes" );
+	
+	//register_taxonomy_for_object_type( "letters", "oik_shortcodes" );
+	//bw_register_field_for_object_type( "letters", "oik_shortcodes" );
+	
+	/**
+	 * oik-faq is defined using oik-types. 
+	 * We need to do extend oik-faq using code that runs after oik-types.
+	 */
+	bw_register_custom_tags( "letters", "oik-faq", "Letters" );
+	bw_register_field_for_object_type( "letters", "oik-faq" );
+	
+	
+	bw_register_field( "_plugin_ref", "noderef", "Component", array( "#type" => array( "oik-plugins", "oik-themes" ), "#multiple" => 5, "#optional" => true ) );
+	
+	bw_register_field_for_object_type( "_plugin_ref", "post" );
+	bw_register_field_for_object_type( "_plugin_ref", "page" );
+	bw_register_field_for_object_type( "_plugin_ref", "oik-faq" );
+	
+}
+
+/**
+ * Implements 'register_post_type_args' for cloning
+ *
+ * This should only be done on the master, not the slave
+ */
+	
+function oik_thugin_register_post_type_args( $args, $post_type ) {
+	
+	$post_types = array( "post", "page", "oik-plugins", "attachment", "oik-themes" );
+	bw_trace2( $post_types, "post_types", false );
+	$add_clone = in_array( $post_type, $post_types );
+	if ( $add_clone ) {
+		$args['supports'][] = 'clone';
+	}
+	bw_trace2( $add_clone, "add_clone", true );
+	return( $args );
+
+}
+
+/**
+ * Enqueues bwlink.css for styling of bobbing wide
+ * 
+ * Note: This is enqueued after oik-custom.css ( priority 12 )
+ */
+function oik_thugin_enqueue_scripts() {
+	$timestamp = null;
+	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+		$timestamp = filemtime( __DIR__ . '/css/oik-thugin.css' );
+	}
+	wp_enqueue_style( 'oik-thugin-css', oik_url( "css/oik-thugin.css", "oik-thugin" ), array(), $timestamp );
+}
+
+/**
+ * Appends more stuff to the footer credits.
+ * 
+ * @param string $text - the footer credits so far
+ * @return string a few additions to brighten the day
+ */
+function oik_thugin_genesis_footer_creds_text( $text ) { 
+	$text .= "[div more][wp v p m][ediv]"; 
+	return( $text );
+}
+
+/**
+ * Adds [bw_fields] for single posts only
+ *
+ * @TODO This could fail if oik is not loaded. 
+ */
+function oik_thugin_genesis_entry_footer() {
+	$post = get_post();
+	if ( $post->post_type == "post" ) {
+		echo bw_do_shortcode( "[bw_fields]" );
+	}
+}
+
+oik_thugin_loaded();
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== oik-thugin ===
+Contributors: bobbingwide
+Donate link: http://www.oik-plugins.com/oik/oik-donate/
+Tags: grandchild, theme, thugin, pleme
+Requires at least: 4.7.3
+Tested up to: 4.7.3
+Stable tag: 0.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+The oik-thugin plugin provides overrides to the genesis-oik theme for oik-plugins.com and oik-plugins.co.uk
+
+- It defines letter taxonomies, additional fields and additional theming logic.
+- It acts as a pseudo grandchild theme
+
+
+
+== Installation ==
+1. Upload the contents of the oik-thugin plugin to the `/wp-content/plugins/oik-thugin' directory
+1. Activate the oik-thugin plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+What's a thugin or pleme?
+
+A thugin is a theme implemented as a plugin.
+A pleme is a plugin with theming capability.
+There could also be a mugin.
+
+What's a pseudo grandchild theme?
+
+A plugin that extends a child theme.
+It's implemented as a plugin since WordPress doesn't support real grandchild themes.
+
+
+
+== Screenshots ==
+1. oik-thugin in action
+
+== Upgrade Notice ==
+= 0.0.1 =
+Improves display of single posts and the overall page layout.
+
+= 0.0.0 =
+New plugin, available from GitHub.
+
+== Changelog ==
+= 0.0.1 = 
+* Changed: Add [bw_fields] to genesis_entry_footer for single-post
+* Changed: Add _plugin_ref to post and page, 
+* Changed: Added fields
+* Changed: Style single post
+* Changed: Cache bust oik-thugin.css when SCRIPT_DEBUG is true
+* Changed: Using priority 12 ensure oik-thugin.css is enqueued after oik-custom.css
+* Changed: Style the bw_logo image
+ 
+= 0.0.0 =
+* Added: New bespoke plugin for bobbingwidewebdesign.com
+
+== Further reading ==
+If you want to read more about the oik plugins then please visit the
+[oik plugin](http://www.oik-plugins.com/oik) 
+**"the oik plugin - for often included key-information"**
+
